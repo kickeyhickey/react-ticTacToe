fa0a86e16c653e81ecc5c9252a6f42a108a09994
kickeyhickey <hickeydeveloper@gmail.com>
2022-08-24 15:31:17 -0700

    added winning logic plus player select

---
+++ src/components/ChoosePlayer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class ChoosePlayer extends Component {
+
+
+    handleForm(e) {
+        e.preventDefault();
+        this.props.setPlayer(e.target.player.value)
+    }
+
+  render() {
+    return (
+      <form onSubmit={(e) => this.handleForm(e)}>
+        <label>
+            Player X
+            <input type="radio" name="player" value="X" />
+        </label>
+        <label>
+            Player O
+            <input type="radio" name="player" value="O" />
+        </label>
+        <input type="submit" value="Start" />
+      </form>
+    )
+  }
+}
