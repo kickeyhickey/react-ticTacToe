d3f6fcef4e40bb79857e431815728fbfdbc45369
kickeyhickey <hickeydeveloper@gmail.com>
2022-08-24 13:43:53 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      board: Array(9).fill(null),
+      player: 'x'
+    }
+  }
+
+  handleClick(id) {
+    const letterBoard = this.state.board
+    letterBoard[id] = this.state.player
+    let newPlayer = this.state.player === "x" ? "o" : "x" 
+   this.setState({ board: letterBoard, player: newPlayer})
+  }
+
+  render() {
+    return (
+      <>
+       <div className='container'>
+         <h1>Tic Tac Tow App</h1>
+         <div className='board'>
+          {this.state.board.map((box, id) => {
+            return (
+              <div 
+              key={id} 
+              className='box' 
+              onClick={() => this.handleClick(id)}>
+                {box}
+              </div>
+            )
+          })}
+          </div>
+      </div>
+      </>
+    )
+  }
 }
-
-export default App;
